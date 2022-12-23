2e53c88da3ffa1ad183dbe4c490889af6be7a24b
kleberlimagit <mov_kleber@hotmail.com>
2022-12-23 10:45:19 -0300

    estrutura inicial

---
+++ projeto-01-front/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./components/Header/Header"
+import './global.css'
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+    <h1>Hello MUNDO</h1>
+
+    </div>
+  )
+}
+
+export default App
---
+++ projeto-01-front/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import style from './Header.module.css'
+
+export function Header(){
+
+    return (
+        <header className={style.header}>
+            <h4>Ignite Feed</h4>
+        </header>
+    );
+}
